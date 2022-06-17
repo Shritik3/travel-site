b2fb7ee6eb01f768bb71df73713c78f544d85089
Shritik <shritikraj@hotmail.com>
2022-06-17 21:33:16 +0530

    Started with modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,9 +3,10 @@ import '../styles/styles.css'
 import RevealOnScroll from './modules/RevealOnScroll'
 import MobileMenu from './modules/MobileMenu'
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 
-
+new Modal();
 let stickyHeader=new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"),75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"),60);
